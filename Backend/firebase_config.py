cdb76bd271d987c3dbd3801d9cdc8f0fc9e78cff
FrankC-12 <facaicedoh@gmail.com>
2024-10-20 18:06:05 -0400

    [UPD] Changes

--- Backend/firebase_config.py
+++ Backend/firebase_config.py
@@ -1,5 +1,5 @@
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("\home\frank\repo\chatbot-6636e-firebase-adminsdk-7k8t2-5c4d9b9807.json")
+cred = credentials.Certificate("/home/frank/repo/chatbot-6636e-firebase-adminsdk-7k8t2-5c4d9b9807.json")
 firebase_admin.initialize_app(cred)
