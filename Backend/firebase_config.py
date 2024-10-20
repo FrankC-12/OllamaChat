27f01c11b6329d282b044eb6bffa1a627837bb9a
FrankC-12 <facaicedoh@gmail.com>
2024-10-20 18:17:46 -0400

    [UPD] Changes

--- Backend/firebase_config.py
+++ Backend/firebase_config.py
@@ -1,5 +1,21 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("/home/frank/repo/chatbot-6636e-firebase-adminsdk-7k8t2-5c4d9b9807.json")
+firebase_credentials = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),  # Esto es necesario para reconstruir los saltos de línea
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
+}
+
+# Inicializar Firebase con las credenciales cargadas
+cred = credentials.Certificate(firebase_credentials)
 firebase_admin.initialize_app(cred)
